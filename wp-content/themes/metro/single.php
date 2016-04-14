00da0773d21895655966b901a428c2a44fb02a05
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2016-04-14 11:29:23 -0700

    refactor post single page

--- wp-content/themes/metro/single.php
+++ wp-content/themes/metro/single.php
@@ -1,59 +1,55 @@
-
 <?php
 /**
- * @package _s
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
  */
 
 get_header(); ?>
-
 <div class="container">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+			// Include the single post content template.
+			get_template_part( 'template-parts/content', 'single' );
 
-		<?php if ( have_posts() ) : ?>
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php if ( has_post_thumbnail() ) {
-						the_post_thumbnail();
-					} ?>
-				<h1 class="entry-title single-title"><?php the_title(); ?></h1>
-				<article class="entry-content">
-					<?php the_content(); ?>
-				</article>
-				<footer class="entry-footer">
-					<span class="cat-links">Posted in&nbsp;<?php the_category(); ?></span>
-					<span class="tagged-links"><?php the_tags('Tagged '); ?></span>
-				</footer>
-
-
-			<?php
+			if ( is_singular( 'attachment' ) ) {
+				// Parent post navigation.
+				the_post_navigation( array(
+					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
+				) );
+			} elseif ( is_singular( 'post' ) ) {
 				// Previous/next post navigation.
 				the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next:', 'twentyfifteen' ) . '</span> ' .
-				'<span class="screen-reader-text">' . __( '', 'twentyfifteen' ) . '</span> ' .
-				'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous:', 'twentyfifteen' ) . '</span> ' .
-				'<span class="screen-reader-text">' . __( '', 'twentyfifteen' ) . '</span> ' .
-				'<span class="post-title">%title</span>',
+					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>',
+					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
+						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
+						'<span class="post-title">%title</span>',
 				) );
-			?>
-			<?php endwhile; ?>
+			}
 
-			
+			// End of the loop.
+		endwhile;
+		?>
 
-		<?php else : ?>
+	</main><!-- .site-main -->
 
-			<?php get_template_part( 'content', 'none' ); ?>
 
-		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	
+</div><!-- .content-area -->
+
 	<?php get_sidebar(); ?>
-
-</div><!-- #container -->
-
+</div><!-- .container -->
 <?php get_footer(); ?>
